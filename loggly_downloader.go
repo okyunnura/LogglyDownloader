49f54c9c0361c1c82296fee89738abce868a10ad
Kenichi Okumura <ken@acryink.com>
2018-02-01 10:43:27 +0900

    feat:add request process

--- loggly_downloader.go
+++ loggly_downloader.go
@@ -4,6 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"log"
 )
 
 type SearchResult struct {
@@ -58,10 +62,14 @@ var token string
 
 var account string
 
+var baseUrl string
+
 func init() {
 	flag.StringVar(&token, "token", "", "loggly api token.")
 	flag.StringVar(&account, "account", "", "loggly account name.")
 	flag.Parse()
+
+	baseUrl = fmt.Sprintf("https://%s.loggly.com/apiv2", account)
 }
 
 func main() {
@@ -77,6 +85,41 @@ func main() {
 	}
 	fmt.Println("account:" + account)
 
+	// TODO params
+	day := "01-30"
+	from := fmt.Sprintf("2018-%sT01:00:00.000Z", day)
+	until := fmt.Sprintf("2018-%sT09:00:00.000Z", day)
 
+	var searchResult SearchResult
+	searchUrl := fmt.Sprintf("/search?q=*&from=%s&until=%s", from, until)
 
+	if err := request(searchUrl, &searchResult); err != nil {
+		fmt.Println("error:search request")
+		log.Fatal(err)
+		os.Exit(0)
+	}
+	fmt.Printf("%q\n", searchResult)
+
+	var tagResult TagResult
+	tagUrl := fmt.Sprintf("/fields/tag?rsid=%s", searchResult.Rsid.ID)
+
+	if err := request(tagUrl, &tagResult); err != nil {
+		fmt.Println("error:tag request")
+		log.Fatal(err)
+		os.Exit(0)
+	}
+	fmt.Printf("%q\n", tagResult)
+
+}
+
+func request(path string, result interface{}) error {
+	fmt.Println("request:" + baseUrl + path)
+	req, _ := http.NewRequest("GET", baseUrl+path, nil)
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := new(http.Client)
+	resp, _ := client.Do(req)
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	return json.Unmarshal(body, &result)
 }
